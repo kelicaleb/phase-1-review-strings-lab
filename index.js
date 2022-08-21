c918d2065625c48645f51b684ba64671f7cce2ed
kelicaleb <kelicaleb7@gmail.com>
2022-08-22 00:33:19 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "Caleb Kavindu"; 
+const welcomeMessage = "welcome to Flattbook" + currentUser + "!";
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = welcomeMessage.slice(0, 7) + "!";
